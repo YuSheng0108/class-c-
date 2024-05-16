2757148170ffd8d10904700654b6061105e9b3e9
YuSheng0108 <s950073255@gmail.com>
2024-05-16 21:10:52 +0800

    game2

--- n09310012/n09310012/game.cs
+++ n09310012/n09310012/game.cs
@@ -12,14 +12,44 @@ namespace n09310012
 {
     public partial class game : Form
     {
+        int min = 0 , max = 100 ;
+        int answer;
         public game()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int input = Int32.Parse(textBox1.Text);
+                if (input == this.answer)
+                {
+                    MessageBox.Show("恭喜答對!");
+                }
+                else if(input < this.answer) 
+                {
+                    this.min = input;
+                }
+                else if (input > this.answer)
+                {  
+                    this.max = input;
+                }
+                label2.Text = string.Format("請輸入 {0}~{1}之間的數值", min, max);
+            } catch(Exception ex)
+            {
+                MessageBox.Show("輸入錯誤，請輸入數字:");
+            }
+        }
+
         private void gstart_Click(object sender, EventArgs e)
         {
-            Random r = new Random();
+            Random r =new Random();
+            this.answer = r.Next(100);
+            MessageBox.Show(answer.ToString());
+            label1.Text = "已隨機產生0~100的數，請在下方輸入:";
+            label2.Text = string.Format("請輸入 {0}~{1}之間的數值",min,max);
         }
     }
 }
